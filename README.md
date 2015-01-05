13391410f2b3b96f934b8a88dc85c77842244115
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-01-05 18:42:14 -0500

    Write page content to file

---
+++ README.md
@@ -0,0 +1,4 @@
+## Tracking changes on Makeyourmove.tv
+
+### Scripting with Ruby
+[Tutorial](http://www.dreamsyssoft.com/ruby-scripting-tutorial/ifelse-tutorial.php)
--- run.rb
+++ run.rb
@@ -1,16 +1,28 @@
-require 'rubygems'
+#!/usr/bin/env ruby
+
 require 'bundler/setup'
 require 'httparty'
 require 'git'
 
 class ChangeTracker
-  def pages 
-    urls = ['http://videos.makeyourmove.tv/']
+  attr_reader :url
+  attr_accessor :file
+
+  def initialize
+    @file = 'index.html' 
+    @url = 'http://videos.makeyourmove.tv/'
   end
 
   def find_changes
-    pages = self.pages
-    puts pages
+    content = HTTParty.get(self.url)
+    file = self.file 
+    
+    File.open("#{file}", 'w') do |f|
+      f.write(content)
+      f.close
+    end
+
+    diff = `git diff #{file}`
   end
 end
 
