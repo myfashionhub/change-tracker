729c9ca9417704b055da7748a04c2a32b9d88894
Nessa Nguyen <v.nessa.nguyen@gmail.com>
2015-01-06 12:01:56 -0500

    Read and eliminate changing line before writing & git diff

--- run.rb
+++ run.rb
@@ -6,37 +6,50 @@ require 'date'
 
 class ChangeTracker
   attr_reader :url
-  attr_accessor :file
+  attr_accessor :file, :draft
 
   def initialize
+    @draft = 'draft.html'
     @file = 'index.html' 
     @url = 'http://videos.makeyourmove.tv/'
   end
 
   def find_changes
     content = HTTParty.get(self.url)
-    file = self.file 
+    file = self.file
+    draft = self.draft 
     message = "Changes detected #{DateTime.now}"
-
-    File.open("#{file}", 'w') do |f|
+    
+    File.open("#{draft}", 'w') do |f|
       f.write(content)
-      f.readlines do |line|
-        line = nil if line.include?('decor/track/dot') || 
-        line.include?('media_player_insertion_')
-      end
       f.close
     end
 
-    diff = `git diff #{file}`
-    unless diff.empty?
-      add = system "git add #{file}"
-      if add 
-        commit = system "git commit -m '#{message}'"
-        if commit 
-          system "git push origin master"
-        end 
-      end    
+    lines = []
+    File.readlines("#{draft}").each do |line|
+      unless line.include?('decor/track/dot') || 
+        line.include?('media_player_insertion_')
+        lines.push(line)
+      end  
     end
+
+    File.open("#{file}", 'w') do |f|
+      lines.each do |line|
+        f.write(line)
+      end
+      f.close
+    end
+    
+    diff = `git diff #{file}`
+    # unless diff.empty?
+    #   add = system "git add #{file}"
+    #   if add 
+    #     commit = system "git commit -m '#{message}'"
+    #     if commit 
+    #       system "git push origin master"
+    #     end 
+    #   end    
+    # end
   end
 end
 
